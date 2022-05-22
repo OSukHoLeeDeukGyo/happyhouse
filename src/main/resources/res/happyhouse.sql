523bec2fb73d321bcebbbf26d6457584f56a4935
Ssoco <dlscjs8646@gmail.com>
2022-05-22 22:09:50 +0900

    documentation: happyhouse.sql 파일에 초기 sql 쿼리문 작성후 첨부

---
+++ src/main/resources/res/happyhouse.sql
@@ -0,0 +1,279 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema ssafyweb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema ssafyweb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `ssafyweb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `ssafyweb` ;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`sidocode`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sidocode` (
+  `sidoCode` VARCHAR(10) NOT NULL,
+  `sidoName` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`sidoCode`),
+  UNIQUE INDEX `sidoName` (`sidoName` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`dongcode`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dongcode` (
+  `dongCode` VARCHAR(10) NOT NULL,
+  `sidoName` VARCHAR(30) NULL DEFAULT NULL,
+  `gugunName` VARCHAR(30) NULL DEFAULT NULL,
+  `dongName` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`dongCode`),
+  INDEX `sidoName` (`sidoName` ASC) VISIBLE,
+  CONSTRAINT `dongcode_ibfk_1`
+    FOREIGN KEY (`sidoName`)
+    REFERENCES `sidocode` (`sidoName`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`baseaddress`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `baseaddress` (
+  `no` INT NOT NULL AUTO_INCREMENT,
+  `sidoName` VARCHAR(30) NULL DEFAULT NULL,
+  `gugunName` VARCHAR(30) NULL DEFAULT NULL,
+  `dongName` VARCHAR(30) NULL DEFAULT NULL,
+  `dongCode` VARCHAR(10) NULL DEFAULT NULL,
+  `lat` VARCHAR(20) NULL DEFAULT NULL,
+  `lng` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`no`),
+  INDEX `dongCode` (`dongCode` ASC) VISIBLE,
+  CONSTRAINT `baseaddress_ibfk_1`
+    FOREIGN KEY (`dongCode`)
+    REFERENCES `dongcode` (`dongCode`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3597
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`ssafy_member`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ssafy_member` (
+  `userid` VARCHAR(16) NOT NULL,
+  `username` VARCHAR(20) NOT NULL,
+  `userpwd` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(50) NULL DEFAULT NULL,
+  `joindate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`board`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `board` (
+  `articleno` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(16) NULL DEFAULT NULL,
+  `subject` VARCHAR(100) NULL DEFAULT NULL,
+  `content` VARCHAR(2000) NULL DEFAULT NULL,
+  `hit` INT NULL DEFAULT '0',
+  `regtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`articleno`),
+  INDEX `board_to_member_fk` (`userid` ASC) VISIBLE,
+  CONSTRAINT `board_to_member_fk`
+    FOREIGN KEY (`userid`)
+    REFERENCES `ssafy_member` (`userid`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 15
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`guestbook`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `guestbook` (
+  `articleno` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(16) NOT NULL,
+  `subject` VARCHAR(100) NOT NULL,
+  `content` VARCHAR(2000) NOT NULL,
+  `regtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`articleno`),
+  INDEX `guestbook_username_FK_idx` (`userid` ASC) VISIBLE,
+  CONSTRAINT `guestbook_userid_FK`
+    FOREIGN KEY (`userid`)
+    REFERENCES `ssafy_member` (`userid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`file_info`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `file_info` (
+  `articleno` INT NOT NULL,
+  `savefolder` VARCHAR(45) NULL DEFAULT NULL,
+  `originfile` VARCHAR(50) NULL DEFAULT NULL,
+  `savefile` VARCHAR(50) NULL DEFAULT NULL,
+  INDEX `file_info_articleno_fk_idx` (`articleno` ASC) VISIBLE,
+  CONSTRAINT `file_info_articleno_fk`
+    FOREIGN KEY (`articleno`)
+    REFERENCES `guestbook` (`articleno`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`guguncode`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `guguncode` (
+  `gugunCode` VARCHAR(10) NOT NULL,
+  `gugunName` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`gugunCode`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`house`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `house` (
+  `built` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL,
+  `dong` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL,
+  `apt` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL,
+  `landnum` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL,
+  `code` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`houseinfo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `houseinfo` (
+  `aptCode` INT NOT NULL,
+  `aptName` VARCHAR(50) NULL DEFAULT NULL,
+  `dongCode` VARCHAR(10) NULL DEFAULT NULL,
+  `dongName` VARCHAR(30) NULL DEFAULT NULL,
+  `buildYear` INT NULL DEFAULT NULL,
+  `jibun` VARCHAR(30) NULL DEFAULT NULL,
+  `lat` VARCHAR(30) NULL DEFAULT NULL,
+  `lng` VARCHAR(30) NULL DEFAULT NULL,
+  `img` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`aptCode`),
+  INDEX `dongCode` (`dongCode` ASC) VISIBLE,
+  CONSTRAINT `houseinfo_ibfk_1`
+    FOREIGN KEY (`dongCode`)
+    REFERENCES `dongcode` (`dongCode`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`housedeal`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `housedeal` (
+  `no` INT NOT NULL AUTO_INCREMENT,
+  `aptCode` INT NULL DEFAULT NULL,
+  `dealAmount` VARCHAR(20) NULL DEFAULT NULL,
+  `dealYear` INT NULL DEFAULT NULL,
+  `dealMonth` INT NULL DEFAULT NULL,
+  `dealDay` INT NULL DEFAULT NULL,
+  `area` VARCHAR(30) NULL DEFAULT NULL,
+  `floor` VARCHAR(30) NULL DEFAULT NULL,
+  `type` VARCHAR(30) NULL DEFAULT NULL,
+  `rentMoney` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`no`),
+  INDEX `aptCode` (`aptCode` ASC) VISIBLE,
+  CONSTRAINT `housedeal_ibfk_1`
+    FOREIGN KEY (`aptCode`)
+    REFERENCES `houseinfo` (`aptCode`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 68865
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`notice`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `notice` (
+  `articleno` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(16) NULL DEFAULT NULL,
+  `subject` VARCHAR(100) NULL DEFAULT NULL,
+  `content` VARCHAR(2000) NULL DEFAULT NULL,
+  `regtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`articleno`),
+  INDEX `notice_userid_fk_idx` (`userid` ASC) VISIBLE,
+  CONSTRAINT `notice_userid_fk`
+    FOREIGN KEY (`userid`)
+    REFERENCES `ssafy_member` (`userid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `ssafyweb`.`qna`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `qna` (
+  `articleno` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(16) NULL DEFAULT NULL,
+  `subject` VARCHAR(100) NULL DEFAULT NULL,
+  `content` VARCHAR(2000) NULL DEFAULT NULL,
+  `hit` INT NULL DEFAULT NULL,
+  `regtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `reply` VARCHAR(2000) NULL DEFAULT NULL,
+  PRIMARY KEY (`articleno`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+insert into qna(`subject`, `userid`, `content`)
+values ('113133', 'ssafy', '테스트내용');
+select * from qna;
+
+select articleno, userid, subject, content, hit, regtime
+	from board 
+	order by articleno desc;
+    
+insert into ssafy_member (userid, userpwd, username, email, joindate)
+values ('admin', '1234', '관리자', 'admin@ssafy.com', now());
+
+insert into ssafy_member (userid, userpwd, username, email, joindate)
+values ('ssafy', 'ssafy', '싸피', 'ssafy@ssafy.com', now());
+    
+insert into board(`subject`, `userid`, `content`)
+values ('113133', 'ssafy', '테스트내용');
+
+select articleno, userid, subject, regtime
+from qna 
+order by articleno desc;
+
+select * from qna;
+
+select * from ssafy_member;
